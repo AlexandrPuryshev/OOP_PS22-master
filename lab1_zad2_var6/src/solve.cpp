0e51b9e48082fe322c93914b9d37efc987a9f169
Kronos <Kronos0041@gmail.com>
2015-03-07 12:06:50 +0300

    Изменен код
    Добавлена проверка от ввода не числовых данны�

--- lab1_zad2_var6/src/solve.cpp
+++ lab1_zad2_var6/src/solve.cpp
@@ -3,6 +3,7 @@
 #include <stdio.h>
 #include <conio.h>
 #include <math.h>
+#include <sstream>
 
 using namespace std;
 
@@ -12,7 +13,7 @@ int iSign(float fValue)
 	return (fValue > 0) - (fValue < 0);
 }
 
-bool bCheckNullElement(float a, float b, float c)
+bool NuLLElementsIsExist(float a, float b, float c) // переименовать функцию, и ввод не числовых даных
 {
 	if (a == 0)
 	{
@@ -48,36 +49,49 @@ bool bCheckNullElement(float a, float b, float c)
 	}
 }
 
+bool isNumber(const std::string &String)
+{
+	size_t offset = 0;
+	if (String[offset] == '-')
+		++offset;
+	return String.find_first_not_of("0123456789", offset) == std::string::npos;
+}
+
 int main(int argc, char* argv[])
 {
 	float a, b, c, D;
-
 	printf("Input polynome coefficientes (ax^2+bx+c=0):\n");
 	if (argc != 4)
 	{
 		cout << "Error: Invalid number of parameters!" << endl;
 		return 0;
 	}
-	a = atoi(argv[1]);
-	b = atoi(argv[2]);
-	c = atoi(argv[3]);
-	if (bCheckNullElement(a,b,c) == true)
+	if ((isNumber(argv[1]) == 1) && (isNumber(argv[2]) == 1) &&
+		(isNumber(argv[3]) == 1))
 	{
-		D = (b*b) - (4*a*c); // discriminant
-		switch (iSign(D))
+		a = atoi(argv[1]);
+		b = atoi(argv[2]);
+		c = atoi(argv[3]);
+		if (NuLLElementsIsExist(a, b, c) == true)
 		{
-		case 0:
-			printf("X=%3.4f\n", (-b / (2 * a))); // one root
-			return 0;
-			break;
-		case 1:
-			printf("X1=%3.4f \nX2=%3.4f\n", (-b + sqrt(D)) / (2 * a), (-b - sqrt(D)) / (2 * a)); // two root
-			getchar();
-			return 0;
-			break;
-		case -1:
-			printf("There is no real root\n"); // there are no roots
-			return 0;
+			D = (b*b) - (4 * a*c); // discriminant
+			switch (iSign(D))
+			{
+			case 0:
+				printf("X=%3.4f\n", (-b / (2 * a))); // one root
+				return 0;
+			case 1:
+				printf("X1=%3.4f \nX2=%3.4f\n", (-b + sqrt(D)) / (2 * a), (-b - sqrt(D)) / (2 * a)); // two root
+				getchar();
+				return 0;
+			case -1:
+				printf("There is no real root\n"); // there are no roots
+				return 0;
+			}
 		}
+	}
+	else
+	{
+		printf("There is no digits!\n");
 	}
 }
