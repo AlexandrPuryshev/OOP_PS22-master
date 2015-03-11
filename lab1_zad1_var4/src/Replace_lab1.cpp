15341787c3c4647b14e5f79d45a6db72450fa34c
Kronos <Kronos0041@gmail.com>
2015-03-11 19:26:52 +0300

    Переделан код, в соответствии под замечания
    плюсами в замечаниях помечено то, что переделал

--- lab1_zad1_var4/src/Replace_lab1.cpp
+++ lab1_zad1_var4/src/Replace_lab1.cpp
@@ -4,34 +4,90 @@
 #include <Windows.h>
 #include <string>
 #include <stdlib.h>
+#include <assert.h>
 
 using namespace std;
 
-
-void ReplaceString(ifstream &fIn, ofstream &fOut, string sSearchString, string sReplaceString, bool &bFlag) // не только замена слова, но и в строке тоже!
+void ReplaceStringsInString(string SearchString, string ReplaceString, string &ReplacerString)
 {
-	while (!fIn.eof() && sSearchString.length() > 0)
+	size_t i, j;
+	size_t iLenStr = SearchString.length();
+	for (i = 0; i <= ReplacerString.size() - SearchString.size(); i++)
 	{
-		string sReplacerString;
-		fIn >> sReplacerString;
-		size_t pos = sReplacerString.find(sSearchString);
-		int iLenStr = sSearchString.length();
-		char chSymbolTrancsfer = fIn.get();
-		if (pos != std::string::npos)
+		for (j = 0; j + 1 < SearchString.size(); j++)
 		{
-			bFlag = true;
-			sReplacerString.replace(pos, iLenStr, sReplaceString);
+			if (ReplacerString[i + j] != SearchString[j]) { break; }
 		}
-		else if (sReplacerString == sSearchString)
+		if (j + 1 == SearchString.size())
 		{
-			bFlag = true;
-			sReplacerString = sReplaceString;
+			ReplacerString.replace(i, j, ReplaceString);
+			i += SearchString.size();
 		}
-		if (chSymbolTrancsfer != EOF)
+	}
+}
+
+void ReplaceStringInFile(ifstream &fIn, ofstream &fOut, string SearchString, string ReplaceString, bool &StringIsFound)
+{
+	while (!fIn.eof() && SearchString.length() > 0)
+	{
+		string ReplacerString;
+		fIn >> ReplacerString;
+		size_t Position = ReplacerString.find(SearchString);
+		char SymbolTrancsfer = fIn.get();
+		if ((Position != std::string::npos) && (ReplacerString.size() > SearchString.size()))
 		{
-			sReplacerString = sReplacerString + chSymbolTrancsfer;
+			StringIsFound = true;
+			ReplaceStringsInString(SearchString, ReplaceString, ReplacerString);
 		}
-		fOut << sReplacerString;
+		else if (ReplacerString == SearchString)
+		{
+			StringIsFound = true;
+			ReplacerString = ReplaceString;
+		}
+		if (SymbolTrancsfer != EOF)
+		{
+			ReplacerString = ReplacerString + SymbolTrancsfer;
+		}
+		fOut << ReplacerString;
+	}
+}
+
+void OpenFilesAndReplaceStrings(string SearchString, string ReplaceString, char* &FileArgument1, char* &FileArgument2)
+{
+	if ((strlen(FileArgument1) > 0) && (strlen(FileArgument2) > 0))
+	{
+		bool StringIsFound = false;
+		ifstream fIn;
+		fIn.open(FileArgument1, ios::in);
+		if (fIn.fail()) // check opening of the file
+		{
+			cout << "Error when opening files of reading" << endl;
+			exit(-1);
+		}
+		else
+		{
+			ofstream fOut;
+			fOut.open((const char*)FileArgument2, ios::out);
+			if (fOut.fail())
+			{
+				cout << "Error when opening files of writing" << endl;
+				exit(-1);
+			}
+			ReplaceStringInFile(fIn, fOut, SearchString, ReplaceString, StringIsFound);
+			if (StringIsFound == false)
+			{
+				cout << "Such line isn't present!" << endl;
+			}
+			else
+			{
+				cout << "Action is executed" << endl;
+			}
+		}
+	}
+	else
+	{
+		cout << "Such file isn't present!" << endl;
+		exit(-1);
 	}
 }
 
@@ -40,59 +96,17 @@ int main(int argc, char* argv[])
 	setlocale(LC_ALL, "Russian");
 	SetConsoleCP(1251);
 	SetConsoleOutputCP(1251);
-
-	if (argc != 5)
+	const int PARAMS_COUNT = 5;
+	if (argc != PARAMS_COUNT)
 	{
 		cout << "Incorrect format of input!" << endl;
 		return 0;
 	}
-	string sSearchString, sReplaceString;
-	sSearchString = (const char*)argv[3];
-	sReplaceString = (const char*)argv[4];
-
-	if ((strlen(argv[1]) > 0) && (strlen(argv[2]) > 0))
-	{
-		bool bFlag = false;
-		ifstream fIn;
-		fIn.open((const char*)argv[1], ios::in);
-		if (fIn.fail()) // check opening of the file
-		{
-			cout << "Error when opening files of reading" << endl;
-			fIn.close();
-			return 0;
-		}
-		else
-		{
-			ofstream fOut;
-			fOut.open((const char*)argv[2], ios::out);
-			if (fOut.fail())
-			{
-				cout << "Error when opening files of writing" << endl;
-				fIn.close();
-				fOut.close();
-				return 0;
-			}
-			ReplaceString(fIn, fOut, sSearchString, sReplaceString, bFlag);
-			if (bFlag == false)
-			{
-				cout << "Such line isn't present!" << endl;
-				fIn.close();
-				fOut.close();
-				return 0;
-			}
-			else
-			{
-				cout << "Action is executed" << endl;
-			}
-			fIn.close();
-			fOut.close();
-			return 0;
-		}
-	}
-	else
-	{
-		cout << "Such file isn't present!" << endl;
-		return 0;
-	}
+	string SearchString, ReplaceString;
+	SearchString = (const char*)argv[3];
+	ReplaceString = (const char*)argv[4];
+	char* FileArgument1 = argv[1];
+	char* FileArgument2 = argv[2];
+	OpenFilesAndReplaceStrings(SearchString, ReplaceString, FileArgument1, FileArgument2);
 	return 0;
 }
