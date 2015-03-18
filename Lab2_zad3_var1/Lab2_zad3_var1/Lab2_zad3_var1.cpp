1150b1cf696d87c105c7d8e8007a88f20624eee5
Kronos <Kronos0041@gmail.com>
2015-03-18 11:41:18 +0300

    Добавлено предложение

--- Lab2_zad3_var1/Lab2_zad3_var1/Lab2_zad3_var1.cpp
+++ Lab2_zad3_var1/Lab2_zad3_var1/Lab2_zad3_var1.cpp
@@ -9,7 +9,7 @@ using namespace std;
 
 int _tmain(int argc, _TCHAR* argv[])
 {
-	string str = "  ";
+	string str = "Hello i am you friend and you support. Do you have any friend?";
 	map<string, int> MassOfWord = FindOfftenWord(str);
 	cout << ShowDictionary(MassOfWord);
 }
