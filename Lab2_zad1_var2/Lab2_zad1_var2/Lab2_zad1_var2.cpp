1b86f53e19acb8f7c69546f1bbf8cf33b360407e
Kronos <Kronos0041@gmail.com>
2015-03-18 10:44:23 +0300

    Недобавленные изменения

---
+++ Lab2_zad1_var2/Lab2_zad1_var2/Lab2_zad1_var2.cpp
@@ -0,0 +1,41 @@
+// Lab2_zad1_var2.cpp: определяет точку входа для консольного приложения.
+//
+
+#include "stdafx.h"
+#include <assert.h>
+#include "VectorProcess.h"
+
+void ReadingVector(vector <double> &Values)
+{
+	copy(istream_iterator<double>(cin), istream_iterator<double>(), back_inserter(Values));
+}
+
+void ShowVector(vector <double> &Numbers)
+{
+	copy(Numbers.begin(), Numbers.end(), ostream_iterator<double>(cout, ", "));
+}
+
+
+int main()
+{
+	vector <double> Numbers;
+	vector <double> Result;
+	setlocale(LC_ALL, "Russian");
+	cout << "Введите последовательность чисел через enter: " << endl;
+	cout << "Чтобы закончить ввод введите не числовой символ!" << endl;
+	ReadingVector(Numbers);
+	assert(!Numbers.empty());
+	SortVectorAscending(Numbers);
+	printf("\n");
+	cout << "sorted source massive: ";
+	ShowVector(Numbers);
+	printf("\n");
+	cout << "Size Source vector: " << Numbers.size() << endl;
+	ProcessVector(Numbers, Result);
+	cout << "result massive after mupltiplication minimal digit on different: ";
+	ShowVector(Result);
+	printf("\n");
+	cout << "Size Result vector: " << Result.size() << endl;
+	getchar();
+	return 0;
+}
---
+++ Lab2_zad1_var2/Lab2_zad1_var2/VectorProcess.h
@@ -0,0 +1,10 @@
+#pragma once
+
+#include "stdafx.h"
+
+using namespace std;
+
+void SortVectorAscending(vector <double> &Values);
+void ReadingVector(vector <double> &Values);
+void ProcessVector(vector <double> &Numbers, vector <double> &Result);
+void ShowVector(vector <double> &Numbers);
