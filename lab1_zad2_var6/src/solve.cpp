3b4da9c144cfdd9c0fe9f32ad8d7a7203358a978
Kronos <Kronos0041@gmail.com>
2015-04-08 14:10:13 +0300

    Немножко переделан код -вместо return 0 : false

--- lab1_zad2_var6/src/solve.cpp
+++ lab1_zad2_var6/src/solve.cpp
@@ -13,34 +13,34 @@ int Sign(const float value)
 	return (value > 0) - (value < 0);
 }
 
-bool NuLLElementsIsExist(const float a, const float b, const float c)
+bool NullElementsIsExist(const float a, const float b, const float c)
 {
 	if (a == 0)
 	{
 		printf("Error: square coefficient is zero!\n");
-		return 0;
+		return false;
 	}
 	if ((b == 0) && (c == 0))
 	{
 		printf("X=0\n");
-		return 0;
+		return false;
 	}
 	if ((b == 0) || (c == 0))
 	{
 		if ((b == 0) && (c < 0))
 		{
 			printf("X1,2=+-%3.4f\n", sqrt(-c / a));
-			return 0;
+			return false;
 		}
 		else if ((c == 0) && (b < 0))
 		{
 			printf("X1=0 \nX2=%3.4f\n", (-b / a));
-			return 0;
+			return false;
 		}
 		else
 		{
 			printf("Error: negative coefficients!\n");
-			return 0;
+			return false;
 		}
 	}
 	else
@@ -53,13 +53,14 @@ bool IsNumber(const string &string)
 {
 	size_t offset = 0;
 	if (string[offset] == '-')
+	{
 		++offset;
+	}
 	return string.find_first_not_of("0123456789", offset) == std::string::npos;
 }
 
 int main(int argc, char* argv[])
 {
-	float a, b, c, D;
 	printf("Input polynome coefficientes (ax^2+bx+c=0):\n");
 	const int PARAMS_COUNT = 4;
 	if (argc != PARAMS_COUNT)
@@ -67,13 +68,13 @@ int main(int argc, char* argv[])
 		cout << "Error: Invalid number of parameters!" << endl;
 		return 0;
 	}
-	if ((IsNumber(argv[1]) == true) && (IsNumber(argv[2]) == true) &&
-		(IsNumber(argv[3]) == true))
+	if ((IsNumber(argv[1])) && (IsNumber(argv[2])) && (IsNumber(argv[3])))
 	{
+		float a, b, c, D;
 		a = static_cast<float>(atoi(argv[1]));
 		b = static_cast<float>(atoi(argv[2]));
 		c = static_cast<float>(atoi(argv[3]));
-		if (NuLLElementsIsExist(a, b, c) == true)
+		if (NullElementsIsExist(a, b, c))
 		{
 			D = (b*b) - (4 * a*c); // discriminant
 			switch (Sign(D))
@@ -90,9 +91,14 @@ int main(int argc, char* argv[])
 					return 0;
 			}
 		}
+		else
+		{
+			return 0;
+		}
 	}
 	else
 	{
 		printf("There is no digits!\n");
+		return 0;
 	}
 }
